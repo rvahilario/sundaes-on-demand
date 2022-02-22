17dab7af9d2b844b788c8535076d2cff5d4ed466
Renan Hilario <rvahilario@gmail.com>
2022-02-22 19:57:19 -0300

    feat: :star2: creating SummaryForm comp

---
+++ src/components/SummaryForm/SummaryForm.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+import { SummaryFormProps } from './types';
+
+export const SummaryForm = ({}: SummaryFormProps) => {
+	return (
+		<Container>
+			<h1>SummaryForm</h1>
+		</Container>
+	);
+};
