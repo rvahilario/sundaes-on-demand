fd57d079b44a8825a926bea05d70609fba149046
Renan Hilario <rvahilario@gmail.com>
2022-02-23 17:49:31 -0300

    config: :wrench: jest configs to use mocks

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	clearMocks: true,
 	collectCoverage: true,
-	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts', './jest.setup.js'],
 	testEnvironment: 'jsdom',
 	// testMatch: ['**/__tests__/**/?(*.)+(spec|test).[tj]s?(x)'],
 	testPathIgnorePatterns: ['/node_modules', '/.next/'],
