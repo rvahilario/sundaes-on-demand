aee4625f2a30116a0f7514ff2efde763e8a4e808
Renan Hilario <rvahilario@gmail.com>
2022-02-23 11:47:50 -0300

    feat: :star2: add popover to SummaryForm

--- src/components/SummaryForm/SummaryForm.tsx
+++ src/components/SummaryForm/SummaryForm.tsx
@@ -4,13 +4,24 @@ import { SummaryFormProps } from './types';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 export const SummaryForm = ({}: SummaryFormProps) => {
 	const [tcChecked, setTcChecked] = useState(false);
 
+	const popover = (
+		<Popover id="terms-popover">
+			<Popover.Body>No ice cream will actually be delivered</Popover.Body>
+		</Popover>
+	);
+
 	const checkboxLabel = (
 		<span>
-			I agree to <span>Terms and Conditions</span>
+			I agree to
+			<OverlayTrigger placement="right" overlay={popover}>
+				<span>Terms and Conditions</span>
+			</OverlayTrigger>
 		</span>
 	);
 
