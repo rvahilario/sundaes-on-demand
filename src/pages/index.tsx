3ac24a88c3a57caa83cadf9c81c58f9e30dce918
Renan Hilario <rvahilario@gmail.com>
2022-02-24 18:45:37 -0300

    del: :fire: removing unused lines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { SummaryForm } from '@/components/SummaryForm';
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import styles from '@/styles/';
 
 export default function Home() {
 	return (
