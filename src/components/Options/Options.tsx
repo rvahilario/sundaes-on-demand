e378d975b916c00dbdc1e086cc56eb19c9dc2b0c
Renan Hilario <rvahilario@gmail.com>
2022-02-26 15:33:58 -0300

    feat: :star2: add server req to Options comp

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -1,5 +1,19 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import { Container } from './styles';
 import { OptionsProps } from './types';
 export const Options = ({ optionType }: OptionsProps) => {
-	return <Container />
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(`http://localhost:3030/${optionType}`)
+			.then((res) => setItems(res.data))
+			.catch((error) => {
+				throw new Error(error);
+			});
+	}, [optionType]);
+
+	return <Container />;
 };
