ad0cebf21da27c79636ef67d69b9b16a65d99e3c
Renan Hilario <rvahilario@gmail.com>
2022-02-26 15:28:43 -0300

    test: :white_check_mark: write initial tests to Options comp

--- src/components/Options/Options.test.tsx
+++ src/components/Options/Options.test.tsx
@@ -11,4 +11,18 @@ const renderOptions = () => {
 };
 
 describe('<Options optionType="scoops" />', () => {
+	it('displays image for each scoop option from server', async () => {
+		renderOptions();
+
+		//find images
+		const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
+		expect(scoopImages).toHaveLength(2);
+
+		// confirm alt text of images
+		const altText = scoopImages.map((element) => {
+			const imageElement = element as HTMLImageElement;
+			return imageElement.alt;
+		});
+		expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+	});
 });
