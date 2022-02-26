aa79503c5ad6b27d9b45212c0cc30a77887bd178
Renan Hilario <rvahilario@gmail.com>
2022-02-26 15:20:29 -0300

    refact: :recycle: fix typo in handler mock

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -5,7 +5,7 @@ export const handlers = [
 	rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
 		return res(
 			ctx.json([
-				{ name: 'Chocalate', imagePath: '/image/chocalate.png' },
+				{ name: 'Chocolate', imagePath: '/image/chocalate.png' },
 				{ name: 'Vanilla', imagePath: '/image/vanilla.png' },
 			]),
 		);
