9cfde3197a27425c387a317dca2c3909f4bb5072
Renan Hilario <rvahilario@gmail.com>
2022-03-01 11:42:25 -0300

    feat: :star2: add Alerts to Options comp

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -5,18 +5,25 @@ import { Container } from './styles';
 import { OptionsProps } from './types';
 import { ScoopOption, ScoopOptionProps } from '@/components/ScoopOption';
 import { ToppingOption } from '@/components/ToppingOption';
+import { AlertBanner } from '@/components/AlertBanner';
 
 export const Options = ({ optionType }: OptionsProps) => {
 	const [items, setItems] = useState([]);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		axios
 			.get(`http://localhost:3030/${optionType}`)
 			.then((res) => setItems(res.data))
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
 			.catch((error) => {
-				throw new Error(error);
+				setError(true);
 			});
 	}, [optionType]);
+
+	if (error) {
+		return <AlertBanner />;
+	}
 
 	// TODO: replace null by topping option
 	const ItemComponent = (
