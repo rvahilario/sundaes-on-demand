fef22e2ae4960a3458b5082203fa606e77748dcb
Renan Hilario <rvahilario@gmail.com>
2022-03-01 11:15:45 -0300

    test: :white_check_mark: reset handler to emulate error res in tests

--- src/components/OrderEntry/OrderEntry.test.tsx
+++ src/components/OrderEntry/OrderEntry.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import Theme from '../../styles/theme';
 import { OrderEntry } from './OrderEntry';
+import { rest } from 'msw';
+import { server } from '@/mocks/server';
 
 const renderOrderEntry = () => {
 	render(
@@ -12,6 +14,15 @@ const renderOrderEntry = () => {
 
 describe('<OrderEntry />', () => {
 	it('Handles error for scoops and toppings routes', async () => {
+		server.resetHandlers(
+			rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
+				res(ctx.status(500));
+			}),
+			rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
+				res(ctx.status(500));
+			}),
+		);
+
 		renderOrderEntry();
 
 		const alerts = await screen.findAllByRole('alert', {
