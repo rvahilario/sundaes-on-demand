7e76d8059fb8ca3da6a8bb2947673ef8b7887fd5
Renan Hilario <rvahilario@gmail.com>
2022-03-01 11:17:19 -0300

    feat: :star2: add AlertBanner comp

---
+++ src/components/AlertBanner/AlertBanner.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import Theme from '../../styles/theme';
+import { AlertBanner } from './AlertBanner';
+
+describe('<AlertBanner />', () => {
+	it('should render AlertBanner', () => {
+		render(
+			<Theme>
+				<AlertBanner />
+			</Theme>,
+		);
+
+		const alertBanner = screen.getByText('AlertBanner');
+
+		expect(alertBanner).toBeInTheDocument();
+	});
+});
---
+++ src/components/AlertBanner/AlertBanner.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+import { AlertBannerProps } from './types';
+
+export const AlertBanner = ({}: AlertBannerProps) => {
+	return (
+		<Container>
+			<h1>AlertBanner</h1>
+		</Container>
+	);
+};
