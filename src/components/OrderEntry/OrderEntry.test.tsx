a7b9c123b5a12a43f16090f8de90925baff47502
Renan Hilario <rvahilario@gmail.com>
2022-03-01 12:10:45 -0300

    fix: :beetle: OrderEntry tests - use waitFor to await responses

--- src/components/OrderEntry/OrderEntry.test.tsx
+++ src/components/OrderEntry/OrderEntry.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Theme from '../../styles/theme';
 import { OrderEntry } from './OrderEntry';
 import { rest } from 'msw';
@@ -15,20 +15,20 @@ const renderOrderEntry = () => {
 describe('<OrderEntry />', () => {
 	it('Handles error for scoops and toppings routes', async () => {
 		server.resetHandlers(
-			rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
-				res(ctx.status(500));
-			}),
-			rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
-				res(ctx.status(500));
-			}),
+			rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
+				res(ctx.status(500)),
+			),
+			rest.get('http://localhost:3030/toppings', (req, res, ctx) =>
+				res(ctx.status(500)),
+			),
 		);
 
 		renderOrderEntry();
 
-		const alerts = await screen.findAllByRole('alert', {
-			name: 'An unexpected error ocurred. Please try again later.',
-		});
+		await waitFor(async () => {
+			const alerts = await screen.findAllByRole('alert');
 
-		expect(alerts).toHaveLength(2);
+			expect(alerts).toHaveLength(2);
+		});
 	});
 });
