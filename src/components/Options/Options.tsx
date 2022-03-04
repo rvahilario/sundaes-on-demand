1b6d27124e9275e6838fcb0de2649544b1a5735b
Renan Hilario <rvahilario@gmail.com>
2022-03-04 00:51:05 -0300

    feat: :star2: add context to Options comp

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useOrderDetails } from '@/contexts/OrderDetails';
 
 import { Container } from './styles';
 import { OptionsProps } from './types';
 import { ScoopOption, ScoopOptionProps } from '@/components/ScoopOption';
 import { ToppingOption } from '@/components/ToppingOption';
 import { AlertBanner } from '@/components/AlertBanner';
+import { Row } from 'react-bootstrap';
+import { pricePerItem } from '@/constants';
 
 export const Options = ({ optionType }: OptionsProps) => {
 	const [items, setItems] = useState([]);
 	const [error, setError] = useState(false);
+	const [orderDetails, updateItemCount] = useOrderDetails();
 
 	useEffect(() => {
 		axios
@@ -29,14 +33,27 @@ export const Options = ({ optionType }: OptionsProps) => {
 	const ItemComponent = (
 		optionType === 'scoops' ? ScoopOption : ToppingOption
 	) as React.ElementType;
+	const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
 	const optionItems = items.map((item: ScoopOptionProps) => (
 		<ItemComponent
 			key={item.name}
 			name={item.name}
 			imagePath={item.imagePath}
+			updateItemCount={(itemName, newItemCount) =>
+				updateItemCount(itemName, newItemCount, optionType)
+			}
 		/>
 	));
 
-	return <Container>{optionItems}</Container>;
+	return (
+		<Container>
+			<h2>{title}</h2>
+			<p>{pricePerItem[optionType]} each</p>
+			<p>
+				{title} total: {orderDetails.totals[optionType]}
+			</p>
+			<Row>{optionItems}</Row>
+		</Container>
+	);
 };
