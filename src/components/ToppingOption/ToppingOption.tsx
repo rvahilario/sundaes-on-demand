f4adab3e736c32866088159a5ff1ce8fe88d7e49
Renan Hilario <rvahilario@gmail.com>
2022-03-04 00:50:03 -0300

    feat: :star2: add context to ToppingOption comp

--- src/components/ToppingOption/ToppingOption.tsx
+++ src/components/ToppingOption/ToppingOption.tsx
@@ -2,11 +2,26 @@
 import { Container } from './styles';
 import { ToppingOptionProps } from './types';
 
-export const ToppingOption = ({ name, imagePath }: ToppingOptionProps) => {
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+export const ToppingOption = ({
+	name,
+	imagePath,
+	updateItemCount,
+}: ToppingOptionProps) => {
+	const handleChange = (event: any) => {
+		updateItemCount(name, event.target.value);
+	};
+
 	return (
 		<Container>
 			<h1>{name}</h1>
 			<img src={imagePath} alt={`${name} topping`} />
+			<Form.Group controlId={`${name}-count`} as={Row}>
+				<Form.Label>{name}</Form.Label>
+				<Form.Control type="number" defaultValue={0} onChange={handleChange} />
+			</Form.Group>
 		</Container>
 	);
 };
--- src/components/ToppingOption/types.ts
+++ src/components/ToppingOption/types.ts
@@ -1,4 +1,5 @@
 export interface ToppingOptionProps {
 	name: string;
 	imagePath: string;
+	updateItemCount: any;
 }
