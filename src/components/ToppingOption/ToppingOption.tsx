be13d4c9ad4f79b42f3cb785834455ae5bb91e9a
Renan Hilario <rvahilario@gmail.com>
2022-03-08 20:18:03 -0300

    feat: :star2: add checkboxes to ToppingOption comp

--- src/components/ToppingOption/ToppingOption.tsx
+++ src/components/ToppingOption/ToppingOption.tsx
@@ -3,24 +3,23 @@ import { Container } from './styles';
 import { ToppingOptionProps } from './types';
 
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
 
 export const ToppingOption = ({
 	name,
 	imagePath,
 	updateItemCount,
 }: ToppingOptionProps) => {
-	const handleChange = (event: any) => {
-		updateItemCount(name, event.target.value);
-	};
-
 	return (
 		<Container>
-			<h1>{name}</h1>
 			<img src={imagePath} alt={`${name} topping`} />
-			<Form.Group controlId={`${name}-count`} as={Row}>
-				<Form.Label>{name}</Form.Label>
-				<Form.Control type="number" defaultValue={0} onChange={handleChange} />
+			<Form.Group controlId={`${name}-topping-checkbox`}>
+				<Form.Check
+					type="checkbox"
+					label={name}
+					onChange={(e) => {
+						updateItemCount(name, e.target.checked ? 1 : 0);
+					}}
+				/>
 			</Form.Group>
 		</Container>
 	);
