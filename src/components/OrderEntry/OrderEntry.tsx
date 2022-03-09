ecff2a3b4a60ad8d19794e76f02d1c36cb6ec825
Renan Hilario <rvahilario@gmail.com>
2022-03-08 21:29:04 -0300

    feat: :star2: add context to OrderEntry comp

--- src/components/OrderEntry/OrderEntry.tsx
+++ src/components/OrderEntry/OrderEntry.tsx
@@ -1,12 +1,16 @@
 import { Container } from './styles';
 import { OrderEntryProps } from './types';
 import { Options } from '@/components/Options';
+import { useOrderDetails } from '@/contexts/OrderDetails';
 
 export const OrderEntry = ({}: OrderEntryProps) => {
+	const [orderDetails] = useOrderDetails();
+
 	return (
 		<Container>
 			<Options optionType="scoops" />
 			<Options optionType="toppings" />
+			<h2>Grand total: {orderDetails.totals.grandTotal}</h2>
 		</Container>
 	);
 };
