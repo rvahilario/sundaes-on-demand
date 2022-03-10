e0f3ef33d9348ff1aed905896413d7c2b24bd2d9
Renan Hilario <rvahilario@gmail.com>
2022-03-10 11:10:10 -0300

    style: :art: improving global styles

--- src/styles/theme/global.ts
+++ src/styles/theme/global.ts
@@ -11,4 +11,8 @@ export default createGlobalStyle`
     color: ${(props) => props.theme.colors.text._300};
     font: 400 16px Roboto, sans-serif;
   }
+
+	h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+}
 `;
