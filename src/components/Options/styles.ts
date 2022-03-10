548ec88ae5c780a9919fcfe9531f50fffd7976f3
Renan Hilario <rvahilario@gmail.com>
2022-03-10 11:11:42 -0300

    style: :art: improving Options comp

--- src/components/Options/styles.ts
+++ src/components/Options/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-	background-color: aqua;
+	background: purple;
+	margin: auto;
+	margin-bottom: 1.5rem;
+	padding: 1rem 2rem;
+	min-width: 320px;
+
+	.row {
+		background: red;
+		justify-content: space-between;
+		margin: auto;
+	}
 `;
