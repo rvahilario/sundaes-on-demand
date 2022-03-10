c7ec2e2014c246bbf65212581803d7768d805c36
Renan Hilario <rvahilario@gmail.com>
2022-03-10 11:12:38 -0300

    style: :art: improving ScoopOption comp

--- src/components/ScoopOption/styles.ts
+++ src/components/ScoopOption/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	max-width: 280px;
-	margin: 1rem 0rem;
+	margin: 1rem 0.8rem;
 	padding: 0.8rem;
 	align-items: center;
 	border: 3px solid ${(props) => props.theme.colors.primary._100};
@@ -18,10 +18,11 @@ export const Container = styled.div`
 	.row {
 		flex-direction: column;
 		padding: 0.5rem 0.5rem;
+		align-items: center;
 		text-align: center;
 
 		.form-control {
-			background-color: ${(props) => props.theme.colors.text._300};
+			background-color: ${(props) => props.theme.colors.text._200};
 			text-align: center;
 			width: 4rem;
 			padding: 0.3rem 0.5rem;
