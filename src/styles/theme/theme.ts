b64b083096d9c73c79119aaebfe90f6ac10d86c1
Renan Hilario <rvahilario@gmail.com>
2022-03-09 21:36:42 -0300

    feat: :star2: add color palette in theme

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,8 +1,26 @@
 const theme = {
 	colors: {
-		background: 'teal',
-		text: 'ivory',
-		primary: '#ed2a4c',
+		text: {
+			_100: '#E7E7E8',
+			_200: '#E2E2E3',
+			_300: '#DCDCDD',
+			_400: '#C8C8C9',
+			_500: '#B6B6B7',
+		},
+		primary: {
+			_500: '#b12b52',
+			_400: '#c32f5a',
+			_300: '#d63463',
+			_200: '#da4671',
+			_100: '#dd577e',
+		},
+		secondary: {
+			_500: '#008E9F',
+			_400: '#009CAF',
+			_300: '#00ACC1',
+			_200: '#17B4C7',
+			_100: '#2CBBCC',
+		},
 	},
 };
 
