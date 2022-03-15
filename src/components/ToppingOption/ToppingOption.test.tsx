71fc49d3ad0328dbb4e11a5571e64cc3b4d3da08
Renan Hilario <rvahilario@gmail.com>
2022-03-14 23:32:56 -0300

    test: :white_check_mark: update ToppingOption comp

--- src/components/ToppingOption/ToppingOption.test.tsx
+++ src/components/ToppingOption/ToppingOption.test.tsx
@@ -1,17 +1,20 @@
-import { render /*screen*/ } from '@testing-library/react';
-import Theme from '../../styles/theme';
+import { render, screen } from '@/test-utils/testing-library-utils';
 import { ToppingOption } from './ToppingOption';
 
 describe('<ToppingOption />', () => {
 	it('should render ToppingOption', () => {
 		render(
-			<Theme>
-				<div />
-			</Theme>,
+			<ToppingOption
+				name="Testing"
+				imagePath="testing.png"
+				updateItemCount={() => true}
+			/>,
 		);
 
-		// const toppingOption = screen.getByText('ToppingOption');
+		const toppingOption = screen.getByText('Testing');
+		const toppingOptionImage = screen.getByAltText('Testing topping');
 
-		// expect(toppingOption).toBeInTheDocument();
+		expect(toppingOption).toBeInTheDocument();
+		expect(toppingOptionImage).toBeInTheDocument();
 	});
 });
