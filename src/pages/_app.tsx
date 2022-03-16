eb9f2491eb56535e4e248cf0af8ac6682b61cc8a
Renan Hilario <rvahilario@gmail.com>
2022-03-15 23:24:23 -0300

    feat: :star2: add title of site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from 'next/app';
 import Theme from '@/styles/theme';
 import { OrderDetailsProvider } from '@/contexts/OrderDetails';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Theme>
 			<OrderDetailsProvider>
+				<Head>
+					<title>Sundae On Demand</title>
+				</Head>
 				<Component {...pageProps} />
 			</OrderDetailsProvider>
 		</Theme>
